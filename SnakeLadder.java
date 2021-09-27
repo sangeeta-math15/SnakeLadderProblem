55e01bda9455c94be97745766f5524d1b5e83546
sngtmth <sngtmth@gmail.com>
2021-09-27 23:57:01 +0530

    added random number between 1 to 6

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -6,6 +6,10 @@ public class SnakeLadder
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to SnakeLadder Problem");
+		System.out.println("Welcome to Player");
+		int number=(int) ((Math.random()*6)+1);
+		System.out.println("num you recieved==>" +number);	
+		
 	}
 
 }
