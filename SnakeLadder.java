81909049d6be5bd0bd68a42031dc500c1186137e
sngtmth <sngtmth@gmail.com>
2021-09-28 00:58:14 +0530

    Player reaches winning position 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -9,34 +9,37 @@ public class SnakeLadder
 		System.out.println("Welcome to Player");
 //		int number=(int) ((Math.random()*6)+1);
 //		System.out.println("num you recieved==>" +number);
-	
-		int choice=(int) ((Math.random()*3)+1);
-		switch(choice)
+		while(position<=100)
 		{
-		case 1:
-			System.out.println("Ladder is here");
-			int num=(int) (Math.random()*6)+1;
-			System.out.println("Number you received==>" +num);
-			position=position+num;
-			break;
-		case 2:
-			System.out.println("Snake is here");
-			int num1=(int) (Math.random()*6)+1;
-			System.out.println("Number you received==>" +num1);
-			if(position<0)
-				position=1;
-			else
-				position=position-num1;
-			if(position==0)
-				position=0;
-			break;
-		default:
-			System.out.println("no switch");
-			break;
-			
-		
+			int choice=(int) ((Math.random()*3)+1);
+			switch(choice)
+			{
+			case 1:
+				System.out.println("Ladder is here");
+				int num=(int) (Math.random()*6)+1;
+				System.out.println("Number you received==>" +num);
+				position=position+num;
+				break;
+			case 2:
+				System.out.println("Snake is here");
+				int num1=(int) (Math.random()*6)+1;
+				System.out.println("Number you received==>" +num1);
+				if(position==0)
+					position=0;
+				else
+					position=position-num1;
+				if(position<0)
+					position=0;
+				break;
+			default:
+				System.out.println("no switch");
+				break;
+					
+			}
+			System.out.println("you are on the position==>" +position);
+			System.out.println();
 		}
-		
+		System.out.println("you are now reached at the position==>" +position);
 	}
 
 }
