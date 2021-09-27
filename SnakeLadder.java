5fc66ba2bc6d1f42079ccb8d766cf54545a5ce06
sngtmth <sngtmth@gmail.com>
2021-09-27 23:41:59 +0530

    added Position

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -2,6 +2,7 @@ package com.SnakeLadder;
 
 public class SnakeLadder 
 {
+	static int position=0;
 	public static void main(String[] args) 
 	{
 		System.out.println("Welcome to SnakeLadder Problem");
