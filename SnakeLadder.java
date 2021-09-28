1e1c838304394b9bd4bb590b3cc3e67f07eeace5
sngtmth <sngtmth@gmail.com>
2021-09-28 15:34:57 +0530

    To get Exact winning Position 100

--- SnakeLadder.java
+++ SnakeLadder.java
@@ -1,46 +1,89 @@
 package com.SnakeLadder;
 
-public class SnakeLadder 
-{
-	static int position=0;
-	public static void main(String[] args) 
-	{
+public class SnakeLadder {
+	static int position = 0;
+	static int new_position = 0;
+	static int number = 0;
+	//static int counter = 0;
+
+	public static void main(String[] args) {
 		System.out.println("Welcome to SnakeLadder Problem");
 		System.out.println("Welcome to Player");
 //		int number=(int) ((Math.random()*6)+1);
 //		System.out.println("num you recieved==>" +number);
-		while(position<=100)
-		{
-			int choice=(int) ((Math.random()*3)+1);
-			switch(choice)
-			{
+		while (position < 100) {
+			int choice = (int) ((Math.random() * 3) + 1);
+			switch (choice) {
 			case 1:
 				System.out.println("Ladder is here");
-				int num=(int) (Math.random()*6)+1;
-				System.out.println("Number you received==>" +num);
-				position=position+num;
+				int number= (int) (Math.random() * 6) + 1;
+				System.out.println("Number you received==>" + number);
+				position = position + number;
+				if (position > 100) {
+					position = position - number;
+
+				}
+				
+
 				break;
 			case 2:
 				System.out.println("Snake is here");
-				int num1=(int) (Math.random()*6)+1;
-				System.out.println("Number you received==>" +num1);
-				if(position==0)
-					position=0;
-				else
-					position=position-num1;
-				if(position<0)
-					position=0;
+				number = (int) (Math.random() * 6) + 1;
+				System.out.println("Number you received==>" + number);
+				if (position == 0) {
+					position = 0;
+				} else {
+					position = position - number;
+				}
+				if (position < 0) {
+					position = 0;
+				}
+				
 				break;
 			default:
 				System.out.println("no switch");
 				break;
-					
+
 			}
-			System.out.println("you are on the position==>" +position);
+			System.out.println("you are on the position==>" + position);
 			System.out.println();
 		}
-		System.out.println("you are now reached at the position==>" +position);
+
+		System.out.println("you are now reached at the position==>" + position);
+		System.out.println();
+
+		if (position > 100)
+
+		{
+			// int counter=0;
+			int choice2 = 0; // die rolled
+			// save the position to new position i.e 95 old save to new position=95
+			new_position = position - number;
+			int number_need = new_position - 100;
+			System.out.println("you are  :" + new_position);
+			System.out.println("you are need :" + number_need);
+			int choice1 = (int) (Math.random() * 3) + 1;
+			System.out.println("you ar get number:" + choice1);
+			if (choice1 != number_need) {
+				// roll the dice
+				choice2 = (int) (Math.random() * 3) + 1;
+				while (choice1 == choice2) {
+					choice2 = (int) (Math.random() * 3) + 1;
+					System.out.println("you ar getting number:" + choice2);
+					if (number_need == choice2) {
+						System.out.println("you are win");
+						System.out.println("you ar getting number:" + choice2);
+						break;
+					} else
+
+						continue;
+
+				}
+			}
+		}
+
+		System.out.println("you are win");
+	
 	}
 
 }
-
